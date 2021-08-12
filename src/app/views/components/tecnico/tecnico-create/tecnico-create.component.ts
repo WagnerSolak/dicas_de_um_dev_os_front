757dc230ef02c7736c52cd5f123af40c00cd4889
= <=>
2021-08-12 09:55:27 -0300

    CREATE para Tecnico implementado

--- src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoCreateComponent implements OnInit {
 
-  constructor() { }
+  tecnico: Tecnico = {
+    id: '',
+    nome: 'Solak',
+    cpf: '799.119.730-18',
+    telefone: '(44)3030-4040'
+  }
+
+  constructor(
+    private router: Router,
+    private service: TecnicoService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  cancel():void{
+    this.router.navigate(['tecnicos'])
+  }
+
+  create(): void {
+    this.service.create(this.tecnico).subscribe((resposta) => {
+      this.router.navigate(['tecnicos'])
+      this.service.message('Técnico criado com sucesso!')
+    }, err => {
+      if(err.error.error.match('já cadastrado')){
+        this.service.message('CPF já cadastrado na base de dados!')
+      }
+    })
   }
 
 }
