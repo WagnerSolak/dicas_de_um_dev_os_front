d89c31b7d08539d036fba8bbecaf7ab909502670
= <=>
2021-08-13 14:32:30 -0300

    CREATE para Os finalizado

--- src/app/views/components/os/os-create/os-create.component.ts
+++ src/app/views/components/os/os-create/os-create.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Cliente } from 'src/app/models/cliente';
+import { OS } from 'src/app/models/os';
 import { Tecnico } from 'src/app/models/tecnico';
 import { ClienteService } from 'src/app/services/cliente.service';
+import { OsService } from 'src/app/services/os.service';
 import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
@@ -11,13 +14,23 @@ import { TecnicoService } from 'src/app/services/tecnico.service';
 })
 export class OsCreateComponent implements OnInit {
 
-  selected = ''
+
+  os: OS = {
+    tecnico: '',
+    cliente: '',
+    observacoes: '',
+    status: '',
+    prioridade: ''
+
+  }
   tecnicos: Tecnico[] = []
   clientes: Cliente[] = []
 
   constructor(
     private tecnicoService: TecnicoService,
-    private clienteService: ClienteService
+    private clienteService: ClienteService,
+    private service: OsService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -34,6 +47,13 @@ export class OsCreateComponent implements OnInit {
   listarClientes(): void {
     this.clienteService.findAll().subscribe((resposta) => {
       this.clientes = resposta;
+    })
+  }
+
+  create(): void {
+    this.service.create(this.os).subscribe((resposta) => {
+      this.service.message("Ordem de Serviço criada com sucesso!")
+      this.router.navigate(['os'])
     })
   }
 
